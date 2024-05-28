5cc521e2b6571899d7340fe0e49d4283a5019604
Бугай Дмитрий Олегович <y1nkee@bk.ru>
2024-05-28 19:51:28 +0300

    test 2.3

--- ftracker.go
+++ ftracker.go
@@ -49,9 +49,9 @@ func ShowTrainingInfo(action int, trainingType string, weight, height, duration 
 
 	switch trainingType {
 	case "Бег":
-		distance := distance(action)                                       // вызовите здесь необходимую функцию
-		speed := meanSpeed(action, duration)                               // вызовите здесь необходимую функцию
-		calories := RunningSpentCalories(action, weight, duration, height) // вызовите здесь необходимую функцию
+		distance := distance(action)                               // вызовите здесь необходимую функцию
+		speed := meanSpeed(action, duration)                       // вызовите здесь необходимую функцию
+		calories := RunningSpentCalories(action, weight, duration) // вызовите здесь необходимую функцию
 		return fmt.Sprintf("Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f\n", trainingType, duration, distance, speed, calories)
 	case "Ходьба":
 		distance := distance(action)                                       // вызовите здесь необходимую функцию
@@ -82,14 +82,10 @@ const (
 // action int — количество совершенных действий(число шагов при ходьбе и беге, либо гребков при плавании).
 // weight float64 — вес пользователя.
 // duration float64 — длительность тренировки в часах.
-func RunningSpentCalories(action int, height, weight, duration float64) float64 {
+func RunningSpentCalories(action int, weight, duration float64) float64 {
 	// ваш код здесь
-	sport := walkingCaloriesWeightMultiplier * weight // 0.035 * ВесСпортсменаКг
-	speed := meanSpeed(action, duration) * kmhInMsec  // Сред. скорость в секунду
-	superSpeed := math.Pow(speed, 2)                  // **сред. скорость в секунду
-	sum := sport + (superSpeed/(height/cmInM))*walkingSpeedHeightMultiplier*weight
-	calBurned := sum * duration * minInH
-	return calBurned
+	speed := meanSpeed(action, duration)
+	return ((runningCaloriesMeanSpeedMultiplier * speed * runningCaloriesMeanSpeedShift) * weight / mInKm * duration * minInH)
 }
 
 // Константы для расчета калорий, расходуемых при ходьбе.
